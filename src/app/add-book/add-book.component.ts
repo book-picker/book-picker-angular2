8d779c1a0434d7c5cf7c604d54dd6cc28cebcfa6
BugOP <surajbbd.in@gmail.com>
2020-10-16 15:21:35 +0530

    Added Mat-Dialog

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Book } from '../book';
-import { BreakpointObserver, Breakpoints,BreakpointState } from '@angular/cdk/layout';
-import { FormGroup, FormBuilder,FormControl, Validators } from '@angular/forms';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GetBookService } from '../services/get-book.service';
-import { GlobalBooks } from '../global-books';
 import { MatDialog } from '@angular/material/dialog';
 import { BookDialogComponent } from '../book-dialog/book-dialog.component';
+import { FormBuilder } from '@angular/forms';
 
 
-interface Genre{
+interface Genre {
   value: string;
-  viewValue : string
+  viewValue: string
 }
 @Component({
   selector: 'app-add-book',
@@ -20,90 +16,98 @@ interface Genre{
   styleUrls: ['./add-book.component.sass']
 })
 export class AddBookComponent implements OnInit {
-  isbn : string;
-  title : string;
-  authors :[];
-  genre : string;
+  isbn: string;
+  title: string;
+  authors: [];
+  genre: string;
   description: string;
-  imageLinks: string;
-  mobile: string;
+  imageLink: string = '../../assets/images/book.svg';
   search_keyword: string;
   items;
   rating;
   display = false;
-  volumeInfo;
-  selectedBookByUser;
-  isExtraSmall: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.XSmall);
+  bookForm;
+  books=[];
   genres: Genre[] = [
-    {value: 'Biography', viewValue: 'Biography'},
-    {value: 'Romantic', viewValue: 'Romantic'},
-    {value: 'Fiction', viewValue: 'Fiction'},
-    {value: 'Sci-Fi', viewValue: 'Sci-Fi'},
-    {value: 'Mystry', viewValue: 'Mystry'},
-    {value: 'Poetry', viewValue: 'Poetry'},
-    {value: 'Education', viewValue: 'Education'},
-    {value: 'Others', viewValue: 'Others'}
+    { value: 'Biography', viewValue: 'Biography' },
+    { value: 'Romantic', viewValue: 'Romantic' },
+    { value: 'Fiction', viewValue: 'Fiction' },
+    { value: 'Sci-Fi', viewValue: 'Sci-Fi' },
+    { value: 'Mystry', viewValue: 'Mystry' },
+    { value: 'Poetry', viewValue: 'Poetry' },
+    { value: 'Education', viewValue: 'Education' },
+    { value: 'Others', viewValue: 'Others' }
   ];
 
-  bookDetails: FormGroup= new FormGroup({});
-  book: FormGroup= new FormGroup({})
-  public newbookDetails: Book = new Book();
-  BreakpointObserver: any;
   constructor(
-    private breakpointObserver: BreakpointObserver,
     public _fb: FormBuilder,
     public _http: HttpClient,
     public bookService: GetBookService,
-    public gb: GlobalBooks,
-    public dialog : MatDialog
-    ){  }  
+    public dialog: MatDialog,
 
-  ngOnInit(){ 
+  ) { }
+
+  ngOnInit() {
+    this.bookForm = this._fb.group({
+      title : [''],
+      authors: [['']],
+      description:[''],
+      rating :[''],
+      ilink : ['../../assets/images/book.svg'],
+      genre : [''],
+      isbn : [''],
+    })
   }
-
-  getBookFuction(){
+ 
+  getBookFuction() {
     this.bookService.GetBooks(this.search_keyword).subscribe(response => {
+      this.books=[];
+      const books_reponse = JSON.parse(JSON.stringify(response)).items
+      for (let book of books_reponse) {
+        this.books.push({
+          title: book.volumeInfo.title,
+          rating: book.volumeInfo.averageRating,
+          authors: book.volumeInfo.authors,
+          imageLink: book.volumeInfo.imageLinks.thumbnail,
+          description: book.volumeInfo.description
+        })
+      }
+
       const dialogRef = this.dialog.open(BookDialogComponent, {
         width: '750px',
-        height : 'auto',
-        data:{data:response},
-        autoFocus:false,
-        });        
-        dialogRef.afterClosed().subscribe(result => {
-        this.selectedBookByUser = result;
-        this.display= true;
-        if (this.selectedBookByUser.volumeInfo.averageRating >=0.1){
-          this.rating = this.selectedBookByUser.volumeInfo.averageRating
+        height: 'auto',
+        data: { data: this.books },
+        autoFocus: false,
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        this.display = true;
+        this.bookForm.patchValue({
+          title : result.title,
+          authors : result.authors,
+          description : result.description,
+          rating : result.rating,
+          ilink : result.imageLink,          
+        })
+        this.imageLink = result.imageLink
+        if (result.rating >=0.1){
+          this.rating = result.rating
         }
       });
+      });
+}
 
-    });
-    
-  }
-  SelectOption(genre){this.genre=genre;}
-  
-  addBookToLibrary(){
-    this.bookDetails = this._fb.group({
-      mobile: ['100'],
-      book: this._fb.group({
-         isbn:[this.isbn],
-         title:[this.title],
-         authors:[this.authors],
-         genre:[this.genre],
-         description:[this.description],
-         rating : [this.selectedBookByUser.volumeInfo.averageRating],
-         imageLinks:[this.selectedBookByUser.volumeInfo.imageLinks.thumbnail]
-      })
-    })
-
-    this.newbookDetails = <Book>this.bookDetails.value;
-    console.log(this.newbookDetails);
+  addBookToLibrary() {
     var Header = new HttpHeaders();
+    console.log(this.bookForm.value)
     Header.append("Content-Type", "application/json").append('Cache-Control', 'no-cache');
-    this._http.post('/', JSON.stringify(this.newbookDetails), { headers: Header }).subscribe(
-    (data) => console.log(data),
-    (response) => console.log(response),
-    // (error) => console.log(error)
+    this._http.post('/', JSON.stringify(
+      {
+        'mobile': '',
+        'book': this.bookForm.value
+      }
+    ), { headers: Header }).subscribe(
+      (data) => console.log(data),
+      (response) => console.log(response),
     )
   }
 
--- src/app/book-dialog/book-dialog.component.ts
+++ src/app/book-dialog/book-dialog.component.ts
@@ -1,42 +1,35 @@
 import { Component, OnInit, inject, Inject, NgModule } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { from } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { title } from 'process';
 @Component({
   selector: 'app-book-dialog',
   templateUrl: './book-dialog.component.html',
   styleUrls: ['./book-dialog.component.sass']
 })
 export class BookDialogComponent implements OnInit {
-items;
-selectedBook;
-  constructor(public dialogRef : MatDialogRef<BookDialogComponent>,
+  
+books=[];
+
+constructor(public dialogRef : MatDialogRef<BookDialogComponent>,
     @Inject (MAT_DIALOG_DATA) public data: any) {
       dialogRef.disableClose = true;
-      const books = JSON.parse(JSON.stringify(data))
-      this.items= books.data.items;
-      console.log(this.items)
+      this.books = data.data
      }
 
-  ngOnInit(): void {
-   
-  }
+  ngOnInit(): void { }
  
   RowSelected(u:any){
-    this.selectedBook = u;
-    this.dialogRef.close(this.selectedBook);
-    
-  }
+    this.dialogRef.close(u);
+    }
+
   Next_button_pressed(){
-    this.selectedBook = {volumeInfo :{
-                              authors:[""],
-                              description:"",
-                              imageLinks:{thumbnail:"../../assets/images/book.svg"},
-                              title:"",
-                              averageRating:"0"
-                            }
-                            };
-    this.dialogRef.close(this.selectedBook);
+    this.dialogRef.close(
+      {
+        title : '',
+        authors : [''],
+        description : '',
+        imageLink : '../../assets/images/book.svg',
+        rating : '0'
+      }
+    );
   }
 }
